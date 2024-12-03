1476a6147346c4c1d5a16bc14e7bd30c6351fd6b
zhongxic <2054939673@qq.com>
2024-12-03 22:42:32 +0800

    统一响应实体封装

---
+++ pkg/result/result.go
@@ -0,0 +1,55 @@
+package result
+
+import "github.com/zhongxic/gin-template/pkg/errorcode"
+
+type Result[T any] struct {
+	Success      bool   `json:"success"`
+	ErrorCode    string `json:"errorCode"`
+	ErrorMessage string `json:"errorMessage"`
+	Data         T      `json:"data"`
+}
+
+func Success() *Result[any] {
+	return &Result[any]{
+		Success:      true,
+		ErrorCode:    "",
+		ErrorMessage: "",
+		Data:         nil,
+	}
+}
+
+func SuccessWithData[T any](data T) *Result[T] {
+	return &Result[T]{
+		Success:      true,
+		ErrorCode:    "",
+		ErrorMessage: "",
+		Data:         data,
+	}
+}
+
+func Failed(code string) *Result[any] {
+	return &Result[any]{
+		Success:      false,
+		ErrorCode:    code,
+		ErrorMessage: "",
+		Data:         nil,
+	}
+}
+
+func FailedWithMessage(code, message string) *Result[any] {
+	return &Result[any]{
+		Success:      false,
+		ErrorCode:    code,
+		ErrorMessage: message,
+		Data:         nil,
+	}
+}
+
+func FailedWithErrorCode(code *errorcode.ErrorCode, args ...any) *Result[any] {
+	return &Result[any]{
+		Success:      false,
+		ErrorCode:    code.Code(),
+		ErrorMessage: code.Message(args...),
+		Data:         nil,
+	}
+}
