17785fa248216608ba682afb040b50db938b3564
zhongxic <2054939673@qq.com>
2024-12-03 22:47:48 +0800

    初始化路由

---
+++ internal/routes/init.go
@@ -0,0 +1,38 @@
+package routes
+
+import (
+	"sync"
+
+	"github.com/gin-gonic/gin"
+	"github.com/zhongxic/gin-template/internal/controller/ping"
+	"github.com/zhongxic/gin-template/pkg/middleware"
+)
+
+var (
+	initOnce sync.Once
+	engine   *gin.Engine
+)
+
+func Init() *gin.Engine {
+	initOnce.Do(func() {
+		engine = initRoutes()
+	})
+	return engine
+}
+
+func initRoutes() *gin.Engine {
+	r := gin.New()
+	registerMiddleware(r)
+	registerRoutes(r)
+	return r
+}
+
+func registerMiddleware(r *gin.Engine) {
+	r.Use(middleware.Logger())
+	r.Use(middleware.Recover())
+}
+
+func registerRoutes(r *gin.Engine) {
+	pingController := &ping.Controller{}
+	r.GET("/ping", pingController.Ping)
+}
