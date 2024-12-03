9b80f5214ee516cdcdbd5e95e63f6651b2ab57cc
zhongxic <2054939673@qq.com>
2024-12-03 22:41:20 +0800

    日志器初始化

---
+++ pkg/logger/logger.go
@@ -0,0 +1,53 @@
+package logger
+
+import (
+	"io"
+	"log/slog"
+	"os"
+	"sync"
+
+	"github.com/zhongxic/gin-template/config"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+var (
+	initOnce  sync.Once
+	closeOnce sync.Once
+	logWriter io.WriteCloser
+)
+
+func Init(logging config.Logging) error {
+	var err error
+	initOnce.Do(func() {
+		err = initLogger(logging)
+	})
+	return err
+}
+
+func initLogger(logging config.Logging) error {
+	logWriter = &lumberjack.Logger{
+		Filename:  logging.File,
+		MaxAge:    logging.MaxAge,
+		MaxSize:   logging.MaxSize,
+		Compress:  true,
+		LocalTime: true,
+	}
+	var logLevel slog.Level
+	if err := logLevel.UnmarshalText([]byte(logging.Level)); err != nil {
+		return err
+	}
+	logger := slog.New(slog.NewJSONHandler(io.MultiWriter(os.Stdout, logWriter), &slog.HandlerOptions{Level: logLevel}))
+	slog.SetDefault(logger)
+	slog.SetLogLoggerLevel(logLevel)
+	return nil
+}
+
+func Close() error {
+	var err error
+	closeOnce.Do(func() {
+		if logWriter != nil {
+			err = logWriter.Close()
+		}
+	})
+	return err
+}
---
+++ pkg/logger/logger_test.go
@@ -0,0 +1,32 @@
+package logger
+
+import (
+	"log/slog"
+	"testing"
+
+	"github.com/zhongxic/gin-template/config"
+)
+
+func TestLogging(t *testing.T) {
+	logging := config.Logging{
+		Level:   "debug",
+		File:    "log.log",
+		MaxAge:  7,
+		MaxSize: 1024,
+	}
+	if err := Init(logging); err != nil {
+		t.Fatal(err)
+	}
+	if logWriter == nil {
+		t.Fatal("the writer has not been initialized")
+	}
+
+	defer func() {
+		_ = Close()
+	}()
+
+	slog.Debug("this is a debug message")
+	slog.Info("this is an info message")
+	slog.Warn("this is a warn message")
+	slog.Error("this is an error message")
+}
